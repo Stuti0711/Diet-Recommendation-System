ed2bbea1b40576c2d56730cbf38933c2e03d2d03
Stuti Nagaich <168558692+Stuti0711@users.noreply.github.com>
2025-03-04 16:27:07 +0530

    Update Diet_Recommendation.py

--- Diet_Recommendation.py
+++ Diet_Recommendation.py
@@ -64,7 +64,7 @@ weight = st.number_input("Enter Weight (kg):", min_value=25.0, max_value=200.0, 
 # Height input with unit selection
 height_unit = st.selectbox("Select Height Unit:", ["Select Unit","Centimeters", "Feet & Inches"], key="height_unit")
 
-if height_unit == "Centimeters","Select Unit":
+if height_unit in ["Centimeters", "Select Unit"]:
     height = st.number_input("Enter Height (cm):", min_value=50.0, max_value=250.0, value=None, key="height_cm", placeholder="Enter height")
 else:
     height_feet = st.number_input("Feet:", min_value=1, max_value=8, value=None, key="height_feet", placeholder="Feet")
