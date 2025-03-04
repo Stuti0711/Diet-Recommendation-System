7323870f4a6b048d0bf7f90d4e0c8587d6717684
Stuti Nagaich <168558692+Stuti0711@users.noreply.github.com>
2025-03-04 15:08:40 +0530

    Update Diet_Recommendation.py

--- Diet_Recommendation.py
+++ Diet_Recommendation.py
@@ -58,7 +58,7 @@ st.title("🥗 Diet Recommendation System")
 gender = st.selectbox("Select Gender:", ["Male", "Female"], index=0, key="gender")
 
 # Weight input (No default value)
-weight = st.number_input("Enter Weight (kg):", min_value=25.0, max_value=200.0, key="weight")
+weight = st.number_input("Enter Weight (kg):", min_value=00.0, max_value=00.0, key="weight")
 
 
 # Height input with unit selection
